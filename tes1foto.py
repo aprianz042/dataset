cc4780f5c36ad1f9ae05e458a4cf0abdd1738989
aprianz042 <aprianz042@gmail.com>
2025-09-06 09:30:32 +0700

    au ah bodo

--- tes1foto.py
+++ tes1foto.py
@@ -2,7 +2,10 @@ import cv2
 #from frontalization import *
 from frontal import *
 
-path = "SAMPLE/3_dataset_affectnet_rafdb_seleksi_wajah_miring/disgust/disgust_0707.jpg"
+#path = "SAMPLE/3_dataset_affectnet_rafdb_seleksi_wajah_miring/disgust/disgust_2859.jpg"
+#path = "SAMPLE/3_dataset_affectnet_rafdb_seleksi_wajah_miring/fear/fear_2073.jpg"
+path = "SAMPLE/3_dataset_affectnet_rafdb_seleksi_wajah_miring/fear/fear_3783.jpg"
+
 
 path = cv2.imread(path)
 #path = cv2.cvtColor(path, cv2.COLOR_BGR2RGB)
